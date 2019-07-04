dde5be0d264bbb6235f2128d65e8bb806b0c1241
Imran Khan <14203054+imrandev@users.noreply.github.com>
2019-07-04 15:27:32 +0600

    add dialog

--- app/src/main/java/com/cliniva/enventory/ui/base/BaseActivity.java
+++ app/src/main/java/com/cliniva/enventory/ui/base/BaseActivity.java
@@ -3,17 +3,14 @@ package com.cliniva.enventory.ui.base;
 /* Created by Imran Khan on 15-Jun-19.
  * Copyright (c) Imran Khan All rights reserved.*/
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import com.cliniva.enventory.R;
 import com.cliniva.enventory.app.InventoryApp;
 
-@SuppressLint("Registered")
 public abstract class BaseActivity extends AppCompatActivity {
 
     public abstract @LayoutRes int getLayoutRes();
